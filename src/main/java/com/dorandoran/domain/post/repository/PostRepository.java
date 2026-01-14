3e9cd8d3274a52c4a057858adcb24fe44352f61a
SH Baek <paek1127@gmail.com>
2026-01-14 16:11:13 +0900

    refactor: n+1 방지 EntityGraph 추가

--- src/main/java/com/dorandoran/domain/post/repository/PostRepository.java
+++ src/main/java/com/dorandoran/domain/post/repository/PostRepository.java
@@ -4,6 +4,7 @@ import com.dorandoran.domain.category.entity.Category;
 import com.dorandoran.domain.post.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -16,16 +17,21 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     void incrementViewCount(@Param("postId") Long postId);
 
     // 기본 전체 조회 (카테고리 + 삭제안된 게시글)
+    @EntityGraph(attributePaths = {"member", "category"})
     Page<Post> findByCategoryAndDeletedAtIsNull(Category category, Pageable pageable);
 
     // 검색별 메서드들
+    @EntityGraph(attributePaths = {"member", "category"})
     Page<Post> findByCategoryAndTitleContainingAndDeletedAtIsNull(Category category, String title, Pageable pageable);
 
+    @EntityGraph(attributePaths = {"member", "category"})
     Page<Post> findByCategoryAndContentContainingAndDeletedAtIsNull(Category category, String content, Pageable pageable);
 
+    @EntityGraph(attributePaths = {"member", "category"})
     Page<Post> findByCategoryAndMember_NicknameContainingAndDeletedAtIsNull(Category category, String nickname, Pageable pageable);
 
     // 전체(제목 OR 내용 OR 작성자)
+    @EntityGraph(attributePaths = {"member", "category"})
     @Query("SELECT p FROM Post p WHERE p.category = :category AND p.deletedAt IS NULL AND (p.title LIKE %:kw% OR p.content LIKE %:kw% OR p.member.nickname LIKE %:kw%)")
     Page<Post> searchAllInCategory(@Param("category") Category category, @Param("kw") String keyword, Pageable pageable);
 }
