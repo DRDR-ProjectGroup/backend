c4949b394006a1c4c68f12ad24989e181718d924
SH Baek <paek1127@gmail.com>
2026-01-26 22:50:39 +0900

    refactor: componet -> configuration 으로 수정

--- src/main/java/com/dorandoran/global/config/WebConfig.java
+++ src/main/java/com/dorandoran/global/config/WebConfig.java
@@ -1,10 +1,10 @@
 package com.dorandoran.global.config;
 
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Component
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
