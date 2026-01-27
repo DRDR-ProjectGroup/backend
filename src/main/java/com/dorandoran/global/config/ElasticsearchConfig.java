8442ac9497528b4920eaf6d5f23ada6a1ecb3e3e
SH Baek <paek1127@gmail.com>
2026-01-27 18:00:13 +0900

    refactor: ES url 환경변수처리

--- src/main/java/com/dorandoran/global/config/ElasticsearchConfig.java
+++ src/main/java/com/dorandoran/global/config/ElasticsearchConfig.java
@@ -5,16 +5,20 @@ import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ElasticsearchConfig {
 
+    @Value("${search.elastic.uris}")
+    private String elasticsearchUris;
+
     @Bean
     public RestClient restClient() {
         return RestClient.builder(
-                HttpHost.create("http://localhost:9200")
+                HttpHost.create(elasticsearchUris)
         ).build();
     }
 
